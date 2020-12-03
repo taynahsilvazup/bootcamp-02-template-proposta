05fb18a5da724d9be81a6e0065b195352e5507e0
taynahsilvazup <taynah.silva@zup.com.br>
2020-12-03 11:10:03 -0300

    dependencias do Spring Cloud OpenFeign

--- src/main/java/br/zup/bootcamp/nossoCartao/NossoCartaoApplication.java
+++ src/main/java/br/zup/bootcamp/nossoCartao/NossoCartaoApplication.java
@@ -2,8 +2,11 @@ package br.zup.bootcamp.nossoCartao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NossoCartaoApplication {
 
 	public static void main(String[] args) {
